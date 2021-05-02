1d1a068b999b94826b7325a3b1b0aca777cdde01
rohithpeddi <rohith.peddi7@gmail.com>
2021-05-02 14:37:53 -0500

    Update demo.py

--- gongvsumexp/demo.py
+++ gongvsumexp/demo.py
@@ -3,4 +3,4 @@ from seq_dpp_linear import SeqDppLinear
 rng_seed = 100
 print("seqDPP-linear on OVP")
 seqDPP = SeqDppLinear(dataset='OVP', rng_seed=rng_seed)
-seqDPP.train_dpp_linear_MLE()
+W, alpha, fval = seqDPP.train_dpp_linear_MLE()
