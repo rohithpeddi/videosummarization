688babd559c33111e9364842aafcaa6c2d7c920e
rohithpeddi <rohith.peddi7@gmail.com>
2021-05-07 07:30:20 -0500

    1

--- gyglivsumexp/summarize.py
+++ gyglivsumexp/summarize.py
@@ -162,7 +162,7 @@ for it in range(max_iterations):
     for shell_index in range(len(shell_types)):
         if shell_index < 3:
             np.random.shuffle(data_files)
-            test_files = data_files[1]
+            test_files = data_files[1:2]
             if shell_index == 0:
                 print("-------------------- OBJ : REPRESENTATIVENESS ----------------------------")
                 weights = [1, 0, 0]
